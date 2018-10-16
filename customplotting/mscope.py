7c263fdb4fe453f1a68037564c8bcd1e906e5ccd
SarthakJariwala <jariwala@uw.edu>
2018-10-16 14:16:17 -0700

    adding dedicated folder and version file

---
+++ customplotting/mscope.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 15 13:16:26 2018
+
+@author: Sarthak
+"""
+
+import os
+import glob
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+
+"""Recylce params for plotting"""
+plt.rc('xtick', labelsize = 20)
+plt.rc('xtick.major', pad = 3)
+plt.rc('ytick', labelsize = 20)
+plt.rc('ytick.major', pad = 3)
+plt.rc('lines', lw = 1.5, markersize = 7.5)
+plt.rc('legend', fontsize = 20)
+
+def plot_confocal(data, FLIM_adjust = True, **kwargs):
+    """
+    Plot confocal PL intensity image.
+    
+    Input:
+        data: numpy array of PL intensities
+        FLIM_adjust: if True, this will plot the image as you see in the FLIM Labview program
+        size: figure size
+        ori: origin of the plot, customisable through matplotlib options
+        color_map: matplotlib colormap
+        min_int: minimum intensity on the colorscale
+        max_int: maximum intensity on the colorscale
+        
+    """
+    
+    color_map = kwargs.get('color_map', None)
+    min_int = kwargs.get('min_int', None)
+    max_int = kwargs.get('max_int', None)
+    ori = kwargs.get('ori', None)
+    size = kwargs.get('size', None)
+    
+    if FLIM_adjust == True:    
+        data = np.transpose(data)
+        
+    plt.figure(figsize=size)
+    plt.imshow(data, origin = ori, cmap = color_map, vmin = min_int, vmax=max_int)
+    plt.colorbar()
+
+def plot_pixera(data, flip_pixera_to_FLIM = True, **kwargs):
+    """
+    Plot pixera images, with the option to adjust the orientation to FLIM image.
+    
+    Input:
+        data: numpy array of PL intensities
+        flip_pixera_to_FLIM: if True, this will plot the image aligned in the same way as the image in FLIM Labview program
+        size: figure size
+        ori: origin of the plot, customisable through matplotlib options
+        color_map: matplotlib colormap
+        min_int: minimum intensity on the colorscale
+        max_int: maximum intensity on the colorscale
+        
+    """
+    
+    color_map = kwargs.get('color_map', None)
+    min_int = kwargs.get('min_int', None)
+    max_int = kwargs.get('max_int', None)
+    ori = kwargs.get('ori', None)
+    size = kwargs.get('size', None)
+    
+    plt.figure(figsize=size)
+    plt.imshow(data, origin = ori, cmap = color_map, vmin = min_int, vmax=max_int)
+    plt.colorbar()
+    
+    if flip_pixera_to_FLIM == True:
+        plt.gca().invert_xaxis()
+        plt.gca().invert_yaxis()
+    
+def Diffusion_plotting(diff_img_path, total_grains, save = False, norm = False):
+    
+    assert type(total_grains) == int, ('Must be integer') 
+    
+    filelist = glob.glob(diff_img_path+'/*.tif')
+    
+    if norm == False:
+        intensity = 255
+    
+    i = 1
+    for filename in filelist:
+        
+        img = cv2.imread(filename,cv2.IMREAD_GRAYSCALE)
+        img = img[590:740, 280:430]
+        
+        plot_pixera(img, flip_pixera_to_FLIM=True, color_map = 'inferno', max_int = intensity)
+        plt.title('Grain #'+filename[-6]+filename[-5], fontsize = 25)
+        
+        if save == True:
+            directory = diff_img_path + '/GrainDiffusion_plotted'
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            #plt.savefig(directory + '/' +'Grain_%.f.tiff' %(i), bbox_inches = 'tight', dpi = 300)
+            plt.savefig(directory + '/' +'Grain_'+filename[-6]+filename[-5]+'.tiff', bbox_inches = 'tight', dpi = 300)
+            plt.close()
+        i +=1
+        
+        if i == total_grains + 1:
+            break
+        else: 
+            continue
+    
+    return
---
+++ customplotting/version.py
@@ -0,0 +1,49 @@
+from __future__ import absolute_import, division, print_function
+from os.path import join as pjoin
+
+# Format expected by setup.py and doc/source/conf.py: string of form "X.Y.Z"
+_version_major = 0
+_version_minor = 1
+_version_micro = ''  # use '' for first of series, number for 1 and above
+# _version_extra = 'dev'
+_version_extra = ''  # Uncomment this for full releases
+
+# Construct full version string from these.
+_ver = [_version_major, _version_minor]
+if _version_micro:
+    _ver.append(_version_micro)
+if _version_extra:
+    _ver.append(_version_extra)
+
+__version__ = '.'.join(map(str, _ver))
+
+CLASSIFIERS = ["Development Status :: 1 - Alpha",
+               "Environment :: Console",
+               "Intended Audience :: Science/Research",
+               "License :: OSI Approved :: MIT License",
+               "Operating System :: OS Independent",
+               "Programming Language :: Python",
+               "Topic :: Scientific/Engineering"]
+
+# Description should be a one-liner:
+description = "customplotting: custom plotting functions"
+# Long description will go up on the pypi page
+# long_description = """
+
+NAME = "customplotting"
+MAINTAINER = "Sarthak"
+MAINTAINER_EMAIL = "jariwala@uw.edu"
+DESCRIPTION = description
+# LONG_DESCRIPTION = long_description
+URL = "https://github.com/SarthakJariwala/Custom-Plotting"
+DOWNLOAD_URL = ""
+LICENSE = "MIT"
+AUTHOR = "SARTHAK"
+AUTHOR_EMAIL = "jariwala@uw.edu"
+PLATFORMS = "OS Independent"
+MAJOR = _version_major
+MINOR = _version_minor
+MICRO = _version_micro
+VERSION = __version__
+# PACKAGE_DATA = {'defectfinder': [pjoin('data', '*')]}
+REQUIRES = ["numpy","opencv-python","matplotlib"]
