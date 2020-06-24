d06593800db4c5b367f3d7a0823036c97b06085a
Sarthak Jariwala <jariwala@uw.edu>
2020-06-24 15:13:50 -0700

    refactor code for better utilization, change some variables to have cleaner input

--- customplotting/general.py
+++ customplotting/general.py
@@ -1,63 +1,130 @@
 import matplotlib.pyplot as plt
 
+def _check_dict(dictionary):
+    if not isinstance(dictionary, dict):
+        raise ValueError(f"{dictionary} must be a dictionary")
 
-def plot_xy(
-    x, y,
-    new_fig=True,
-    **kwargs
-):
-    """
-    Plot X-Y data.
 
-    Input:
-        x: x data
-        y: y data
-        new_fig: if True, it will create a new figure to plot data
-        figsize: figure size
-        xlabel: X-axis label
-        ylabel: Y-axis label
-        axis_label_fontsize: Fontsize for X and Y-axis labels
-        axis_label_fontweight: Fontweight for X and Y-axis labels
-        title: plot title
-        **kwargs: all matplotlib "plot" kwargs
-    """
-
-    xlabel = kwargs.get('xlabel', None)
-    ylabel = kwargs.get('ylabel', None)
-    axis_label_fontsize = kwargs.get('axis_label_fontsize', 30)
-    axis_label_fontweight = kwargs.get('axis_label_fontweight', 'bold')
-    figsize = kwargs.get('figsize', None)
-    title = kwargs.get('title', None)
-
-    label = kwargs.get('label', None)
-
+def _setup_figure(new_fig=True, figsize=None, title=None,
+                  fontdict=None, tick_parameters=None):
+    """Wrapper to setup figure with the desired parameters"""
     if new_fig:
         plt.figure(figsize=figsize)
-        plt.tick_params(direction='out', length=8, width=3.5)
+        if tick_parameters is None: # setup some defaults
+            tick_parameters = {}
+        
+        _check_dict(tick_parameters)
+        
+        tick_parameters.setdefault('direction', 'out')
+        tick_parameters.setdefault('length', 8)
+        tick_parameters.setdefault('width', 3.5)
+        
+        plt.tick_params(**tick_parameters)
+    
+    if fontdict is None:
+        fontdict={}
+    
+    _check_dict(fontdict)
+    
+    fontdict.setdefault('fontsize', 30)
+    fontdict.setdefault('fontweight', 'bold')
 
-    plt.plot(x, y, **kwargs)
-
-    if xlabel is not None:
-        plt.xlabel(
-            xlabel,
-            fontsize=axis_label_fontsize,
-            fontweight=axis_label_fontweight)
-    
-    if ylabel is not None:
-        plt.ylabel(
-            ylabel,
-            fontsize=axis_label_fontsize,
-            fontweight=axis_label_fontweight)
-    
-    if label is not None:
-        plt.legend()
-    
     if title is not None:
         plt.title(
             title,
-            fontsize=axis_label_fontsize,
-            fontweight=axis_label_fontweight)
+            fontdict=fontdict)
+
+
+def _setup_labels(xlabel=None, ylabel=None, label=None, fontdict=None):
+
+    if fontdict is None:
+        fontdict={}
+    
+    _check_dict(fontdict)
+
+    fontdict.setdefault('fontsize', 25)
+    fontdict.setdefault('fontweight', 'bold')
+
+    if xlabel is not None:
+        plt.xlabel(
+            xlabel, fontdict=fontdict)
+
+    if ylabel is not None:
+        plt.ylabel(
+            ylabel, fontdict=fontdict)
+
+    if label is not None:
+        plt.legend()
+
+
+def plot_xy(
+    *args,
+    new_fig=True,
+    figsize=None,
+    tick_parameters=None,
+    xlabel=None,
+    ylabel=None,
+    ax_fontdict=None,
+    title=None,
+    title_fontdict=None,
+    **kwargs
+):
+    """
+    Plot x-y data with specified parameters.
+
+    Parameters
+    ----------
+    x, y : array-like or scalar
+            The horizontal / vertical coordinates of the data points.
+            *x* values are optional and default to `range(len(y))`.
+
+            Commonly, these parameters are 1D arrays.
+
+            They can also be scalars, or two-dimensional (in that case, the
+            columns represent separate data sets).
+
+            These arguments cannot be passed as keywords.
+    
+    new_fig : bool
+        Create a new matplotlib figure. Default is True
+    
+    figsize: iterable, optional
+        Figure size
+    
+    xlabel, ylabel: str, optional
+        X-axis and Y-axis label
+    
+    ax_fontdict: dict, optional
+        Font dictionary for X and Y axis labels
+        
+    title: str, optional
+        Title for the plot
+    
+    title_fontdict: dict, optional
+        Font dictionary for plot title
+
+    Other Paramters
+    ---------------
+    **kwargs: optional
+        All `matplotlib.pyplot.plot` kwargs used to specify line label (for
+        auto legends), linewidth, antialiasing, marker face color.
+    """
+
+    label = kwargs.get('label', None)
+
+    _setup_figure(
+        new_fig=new_fig,
+        figsize=figsize,
+        title=title,
+        fontdict=title_fontdict,
+        tick_parameters=tick_parameters)
+
+    plt.plot(*args, **kwargs)
+
+    _setup_labels(xlabel, ylabel, label, fontdict=ax_fontdict)
+
     plt.tight_layout()
+
 
 def save_fig(filename, dpi=300):
     """Save figure
